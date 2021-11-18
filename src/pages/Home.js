e7d0146154114a7bd3377e6cb30e4efc2516b12a
Drl Khr <arulkhair@gmail.com>
2021-11-18 13:22:03 +0800

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12 bg-gray-100">
+    <section className="max-w-12xl px-4 sm:px-6 lg:px-24 py-12 bg-gray-100">
       <div className="w-full text-center pb-8">
         <div class="grid justify-items-stretch">
           <img
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
 
         <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900 pb-2">
-          Al-Quran Indonesia
+          Al-Qur'an Indonesia
         </h1>
         <p className="text-gray-400 font-normal text-base">
           Baca Al-Qur'an Secara Online dan Mudah
@@ -27,7 +27,10 @@ export default function Home() {
       >
         {[1, 2, 3, 4, 5].map((item, index) => {
           return (
-            <div className="bg-white rounded-lg p-6" key={index}>
+            <div
+              className="bg-white rounded-lg p-6 cursor-pointer hover:bg-blue-50"
+              key={index}
+            >
               <div className="flex items-center space-x-6 mb-4">
                 <div className="h-20 w-20  rounded-full bg-gray-100 vertical-text-center text-center place-items-center flex justify-center">
                   <h1 className="text-3xl text-gray-800 uppercase tracking-wide text-center">
