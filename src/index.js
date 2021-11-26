6a49ee558fe598431f84a90fcdeb8a3acfe8a6b5
Drl Khr <arulkhair@gmail.com>
2021-11-26 11:54:08 +0800

    add pwa config

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <BrowserRouter>
     <App />
@@ -16,3 +16,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
